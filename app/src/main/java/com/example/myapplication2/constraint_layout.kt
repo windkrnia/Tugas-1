bd1dc4aa85f25efff0e0922d3aa874f326bb512c
windakurniaakmila <windakurniaakmila@gmail.com>
2023-01-03 18:08:38 +0700

    Add Tugas-1

---
+++ app/src/main/java/com/example/myapplication2/constraint_layout.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class constraint_layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
